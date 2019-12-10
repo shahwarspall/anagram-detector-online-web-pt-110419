35ecbaa50c025c0d66d5e4bd1da53330ce6f8d41
Shahwar Spall <shevy.spall@gmail.com>
2019-12-10 01:38:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  
+  attr_accessor :whole_word
+  @@all =[]
+  
+  def initialize(whole_word)
+    @whole_word = whole_word
+   
+  end 
+  
+  def match(words)
+    
+    words.find_all do |word| 
+      if word.split("").sort == self.whole_word.split("").sort 
+        word 
+        
+      end 
+      
+    #binding.pry 
+    
+  end 
+end 
+end 
