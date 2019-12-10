917f53e53ae5d86069580cccb1ec8bcd8b3d240d
Shahwar Spall <shevy.spall@gmail.com>
2019-12-10 01:31:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  
+  attr_accessor :word 
+  @@all =[]
+  
+  def initialize(whole_word)
+    @whole_word = whole_word
+    @@all << self 
+    
+  end 
+  
+  def match(words)
+    
+    words.find_all do |word|
+      
+     if word.split(",").sort == self.whole_word.split(",").sort
+       word
+      
+    #binding.pry 
+    
+  end 
+  
+  end 
+  
+end 
